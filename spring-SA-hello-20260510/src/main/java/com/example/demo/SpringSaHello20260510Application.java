402f3948934b57b83cbfe2f9feb4dd7062078267
sato-yugo-12189 <734-student018@la-study.com>
2026-05-14 13:28:20 +0900

    新規作成

---
+++ spring-SA-hello-20260510/src/main/java/com/example/demo/SpringSaHello20260510Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSaHello20260510Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSaHello20260510Application.class, args);
+	}
+
+}
